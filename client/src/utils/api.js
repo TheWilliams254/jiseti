d93c4782ccf1ff3cb4069e44c9ae4909225045ef
william <williamwambugu663@gmail.com>
2025-08-01 16:11:18 +0300

    live server link

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://jiseti-2.onrender.com/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://jiseti-backend-t74m.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
